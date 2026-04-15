f35874222c56cc4741f0bc631aa2507da49cf284
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-15 16:35:35 -0700

    Added copyright

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,6 +15,7 @@ export default function Layout() {
                 <a href = "https://www.instagram.com/k.ellerphotos/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/instagram.svg" alt = "Visit Instagram Page"></img></a>
                 <a href = "https://www.facebook.com/Kassandra-Eller-Photography-104628328407197/about/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/facebook.svg" alt = "Visit Facebook Page"></img></a>
                 <a href = "https://www.linkedin.com/in/kassandra-eller-411356195" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/linkedin.svg" alt = "Visit LinkedIn Profile"></img></a>
+                <p className = "copyright">© Kassandra Eller 2026</p>
             </footer>
         </>
     );
