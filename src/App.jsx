309a9b74e814fc9200772b69dfd8f229e088109f
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-16 15:34:08 -0700

    Reactivated "Exhibition" routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import PersonalWork from "./pages/PersonalWork";
 import PersonalWorkProject from "./pages/PersonalWorkProject";
-// import ExhibitsCategory from "./pages/ExhibitsCategory";
-// import ExhibitIndividual from "./pages/ExhibitIndividual";
+import ExhibitsCategory from "./pages/ExhibitsCategory";
+import ExhibitIndividual from "./pages/ExhibitIndividual";
 import About from "./pages/About";
 
-import EditorialWork from "./pages/EditorialWork";
-import EditorialWorkItem from "./pages/EditorialWorkItem";
+// import EditorialWork from "./pages/EditorialWork";
+// import EditorialWorkItem from "./pages/EditorialWorkItem";
 
 export default function App() {
   return (
@@ -18,12 +18,13 @@ export default function App() {
           <Route index element = {<HomePage />} />
           <Route path = "/work" element = {<PersonalWork />} />
           <Route path = "/work/:slug" element = {<PersonalWorkProject />} />
-          {/* <Route path = "/exhibits"  element = {<ExhibitsCategory />} />
-          <Route path = "/exhibits/:slug" element = {<ExhibitIndividual />} /> */}
+          <Route path = "/exhibits"  element = {<ExhibitsCategory />} />
+          <Route path = "/exhibits/:slug" element = {<ExhibitIndividual />} />
+          <Route path = "/about" element = {<About />} />
+          
           {/* Editorial work hidden for the foreseeable future */}
           {/* <Route path = "editorial" element = {<EditorialWork />} />
           <Route path = "editorial/:slug" element = {<EditorialWorkItem />} /> */}
-          <Route path = "/about" element = {<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
