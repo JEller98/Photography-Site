17a2012a8f692af99e80c591e8b5cafe2828844d
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-15 18:01:01 -0700

    Updated spacing on copyright statement

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,10 +11,12 @@ export default function Layout() {
                 <Outlet />
             </main>
             <footer>
-                {/* social links here */}
-                <a href = "https://www.instagram.com/k.ellerphotos/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/instagram.svg" alt = "Visit Instagram Page"></img></a>
-                <a href = "https://www.facebook.com/Kassandra-Eller-Photography-104628328407197/about/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/facebook.svg" alt = "Visit Facebook Page"></img></a>
-                <a href = "https://www.linkedin.com/in/kassandra-eller-411356195" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/linkedin.svg" alt = "Visit LinkedIn Profile"></img></a>
+                <div className = "footer-social">
+                    {/* social links here */}
+                    <a href = "https://www.instagram.com/k.ellerphotos/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/instagram.svg" alt = "Visit Instagram Page"></img></a>
+                    <a href = "https://www.facebook.com/Kassandra-Eller-Photography-104628328407197/about/" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/facebook.svg" alt = "Visit Facebook Page"></img></a>
+                    <a href = "https://www.linkedin.com/in/kassandra-eller-411356195" target = "_blank" rel = "noopener noreferrer"><img src = "/images/footer/linkedin.svg" alt = "Visit LinkedIn Profile"></img></a>
+                </div>
                 <p className = "copyright">© Kassandra Eller 2026</p>
             </footer>
         </>
