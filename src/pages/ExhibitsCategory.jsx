2f05874b0297c4f5e528e6f6fc1bab30cf07ef36
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-16 16:29:50 -0700

    Added list of exhibits based on JSON data

--- src/pages/ExhibitsCategory.jsx
+++ src/pages/ExhibitsCategory.jsx
@@ -1,6 +1,18 @@
+import {Link} from "react-router-dom";
+import exhibitsList from "../data/exhibits.json";
+import {Helmet} from "react-helmet-async";
+
 export default function ExhibitsCategory () {
     return (
         <>
+            <Helmet><title>Exhibits - Kassandra Eller</title></Helmet>
+            <ul>
+                {exhibitsList.map(exhibit => 
+                    <li key = {exhibit.slug}>
+                        <Link to = {`/exhibits/${exhibit.slug}`}>{exhibit.title}</Link>
+                    </li>
+                )}
+            </ul>
         </>
     );
 }
