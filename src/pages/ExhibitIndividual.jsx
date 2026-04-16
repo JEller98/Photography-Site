69fc22961167395a06cbbcb720a51e251bfce56d
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-16 16:28:32 -0700

    Updated page to display information and images

--- src/pages/ExhibitIndividual.jsx
+++ src/pages/ExhibitIndividual.jsx
@@ -1,6 +1,34 @@
+import {useParams} from "react-router-dom";
+import exhibitsList from "../data/exhibits.json"
+import {Helmet} from "react-helmet-async";
+
 export default function ExhibitIndividual() {
+    const {slug} = useParams();
+    const exhibit = exhibitsList.find(e => e.slug);
+
+    if(!exhibit) {
+        return <p>Exhibit not found.</p>;
+    }
+
     return (
         <>
+            <Helmet><title>{exhibit.title} - Kassandra Eller</title></Helmet>
+            
+            <p className = "project-meta">{exhibit.venue}</p>
+            <p className = "project-meta">{exhibit.date}</p>
+            <a href = {exhibit.artist_statement} target = "_blank" 
+            rel = "noopener noreferrer" className = "project-meta">Artist Statement</a>
+
+            { /* pulling info from the JSON */}
+            <div className = "image-grid">
+                {exhibit.images.map((image, index) => (
+                    <button key = {index} 
+                    onClick = {() => {setIndex(index); setOpen(true);}} 
+                    className = "image-card" tabIndex = {0}>
+                        <img src = {image} alt = {exhibit.alt_text?.[index] ?? ""}/>
+                    </button>
+                ))}
+            </div>
         </>
     );
 }
