047a12e8d8ad98c4603c8fcb0a264d51714b7e2a
jEllerGRC <eller.jared@student.greenriver.edu>
2026-04-15 18:01:29 -0700

    Added HTML title to page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,9 @@
+import { Helmet } from "react-helmet-async";
+
 export default function About() {
     return (
         <>
+            <Helmet><title>About - Kassandra Eller</title></Helmet>
             <p>
                 Kassandra Eller is a visual artist who experiments with how photographic processes intertwine with the ideas of how one views family, nature, and life itself. She strives to reveal the intricacies of how each being reacts to strife and trauma in her works.
             </p>
